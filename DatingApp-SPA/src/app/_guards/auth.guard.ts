ca618047f698e90ade77959a964c52d3ba10f7f5
S-MONEY\abdellah.grib <abdellah.grib_ext@s-money.fr>
2019-02-28 11:10:59 +0100

    End of section 7

---
+++ DatingApp-SPA/src/app/_guards/auth.guard.ts
@@ -0,0 +1,19 @@
+import { AlertifyService } from './../_services/alertify.service';
+import { AuthService } from './../_services/auth.service';
+import { Injectable } from '@angular/core';
+import { CanActivate, Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authSerivce: AuthService, private router: Router, private alertify: AlertifyService) {}
+  canActivate(): boolean {
+    if (this.authSerivce.loggedIn()) {
+      return true;
+    }
+    this.alertify.error('You shall not pass!');
+    this.router.navigate(['/home']);
+    return false;
+  }
+}
