5bbe2ce3e4e488125bdd3deb7bd26ea56b669805
S-MONEY\abdellah.grib <abdellah.grib_ext@s-money.fr>
2019-02-28 10:21:34 +0100

    End of serction 6

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -1,12 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs/operators';
+import { JwtHelperService } from '@auth0/angular-jwt';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   baseUrl = 'http://localhost:5000/api/auth/';
+  jwtHelper = new JwtHelperService();
+  docodedToken: any;
+
   constructor(private http: HttpClient) { }
 
   login(model: any) {
@@ -16,6 +20,8 @@ export class AuthService {
           const user = response;
           if (user) {
             localStorage.setItem('token', user.token);
+            this.docodedToken = this.jwtHelper.decodeToken(user.token);
+            console.log(this.docodedToken);
           }
         })
       );
@@ -23,5 +29,10 @@ export class AuthService {
 
   register(model: any) {
     return this.http.post(this.baseUrl + 'register', model);
+  }
+
+  loggedIn() {
+    const token = localStorage.getItem('token');
+    return !this.jwtHelper.isTokenExpired(token);
   }
 }
